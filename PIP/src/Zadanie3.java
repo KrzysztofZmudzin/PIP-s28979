ed81a5499052bd34ff389ba37411ea23c7d94789
Krzysztof Żmudzin <s28979@pjwstk.edu.pl>
2022-11-06 11:02:09 +0100

    zadania

---
+++ PIP/src/Zadanie3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Zadanie3 {
+    public static void main(String[] args) {
+
+        System.out.println("Podaj X: ");
+        System.out.println("Podaj Y: ");
+
+        Scanner scanner= new Scanner(System.in);
+        int X= scanner.nextInt();
+        int Y= scanner.nextInt();
+
+        System.out.println(X);
+        System.out.println(Y);
+    }
+}

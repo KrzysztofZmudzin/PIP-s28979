1a5b524bb3590616d34abdb00699794ec7b262af
s28979 <s28979@pjwstk.edu.pl>
2022-12-08 10:19:00 +0100

    All done

--- PIP5/src/Zadanie2.java
+++ PIP5/src/Zadanie2.java
@@ -1,25 +1,2 @@
-import java.util.Scanner;
-
-public class Zadanie2 {
-    public static void main(String[] args) {
-
-        System.out.println("Podaj wyraz ciagu do obliczenia: ");
-        Scanner scanner= new Scanner(System.in);
-        int wyrazCiagu= scanner.nextInt();
-    }
-
-    public static int obliczCiag(int n){
-
-        int wynik;
-        int counter= 0;
-        if (n>0){
-            while (counter < n){
-                wynik=
-            }
-        }else if (n<0){
-
-        }else {
-            System.out.println("0");
-        }
-    }
+package PACKAGE_NAME;public class Zadanie2 {
 }
